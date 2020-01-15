a983825dc51aaec38ee93b0c8a2849bf0b759324
desperatecat <376198935@qq.com>
2020-01-14 20:26:45 -0500

    useState Hook

---
+++ booklist_react_context_hooks_app/src/components/SongList.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import uuid from "uuid/vi";
+
+const SongList = () => {
+  const [songs, setSongs] = useState([
+    { title: "almost home", id: 1 },
+    { title: "memory gospel", id: 2 },
+    { title: "this wild darkness", id: 3 }
+  ]);
+
+  const addSong = () => {
+    setSongs([...songs, { title: "new song", id: uuid() }]);
+  };
+
+  return (
+    <div className="song-list">
+      <ul>
+        {songs.map(song => {
+          return <li key={song.id}>{song.title}</li>;
+        })}
+      </ul>
+      <button onClick={addSong}>Add a new song</button>
+    </div>
+  );
+};
+
+export default SongList;
